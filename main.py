cb22ddc8bb2f2c8fef480ef5f4a31f19494ad7f3
Maksym <maksym.skorobahatko@protonmail.com>
2022-06-06 16:44:25 +0300

    patch 1.01

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import numpy as np
 import lab2
 
-def krylov(a: np.ndarray):
-    np.set_printoptions(precision=5)
-    n = a.shape[0]
-    y = np.zeros(n + 1, n)
+def krylov(A: np.ndarray):
+    np.set_printoptions(precision = 5)
+    n = A.shape[0]
+    y = np.zeros((n + 1, n))
     y[0][0] = 1
     for i in range(1, n + 1):
-        y[i] = a@y[i - 1]
+        y[i] = A@y[i - 1]
     print('y = ', y)
-    b = y[n]
+    B = y[n]
     y = np.transpose(y[0:n])
     y = y[:, ::-1]
-    p = lab2.gaus(y.tolist(), b.tolist())
+    p = lab2.gaus(y.tolist(), B.tolist())
     print('p = ', p)
     p = np.array(p)
     l = np.roots(np.concatenate(([1], -1 * p)))
@@ -20,13 +20,13 @@ def krylov(a: np.ndarray):
 
 def main():
     np.set_printoptions(precision=5, suppress = True)
-    a = np.array([
+    A = np.array([
         [6.00, 1.10, 0.97, 1.24],
         [1.10, 4.00, 1.30, 0.16],
         [0.97, 1.30, 5, 2.10],
         [1.24, 0.16, 2.10, 7.0]
     ])
-    krylov(a)
+    krylov(A)
 
 if __name__ == '__main__':
     main()
